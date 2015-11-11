848d68bb88ffce4b74d7c31a224400c4db3e8365
Claudia Gheorghe <claudia.gheorghe@he-arc.ch>
2015-11-11 11:59:05 +0100

    Add Card/Selection models

--- app/Card.php
+++ app/Card.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
-    //
+    protected $table='cards';
+    protected $primaryKey='pk_id';
+    protected $fillable=['name', 'image', 'decks_id'];
+    
+    public function selection()
+    {
+        return $this->belongsTo('App\Selection', 'card_id' );
+    }
+    
+    public function decks()
+    {
+        return $this->hasMany('App\Deck', 'decks_id');
+    }
+    
+    public function games()
+    {
+        return $this->belongsToMany('App\Game');
+    }
 }
