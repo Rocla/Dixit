865833df9cce7356987abce6db549bbf18cbffb9
Romain Claret <contact@rocla.ch>
2015-11-18 20:03:58 +0100

    quickfix: forgot to put the username in the menu with the profile arrow

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -41,7 +41,9 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" role="button">
-                                <span class="caret">{{ Auth::user()->name }}</span>
+                                Hello, {{ Auth::user()->username }}
+                                <span class="caret"></span>
+
                             </a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('#') }}">My Profile</a></li>
