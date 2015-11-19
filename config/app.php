d0ac5a434df399dc18ab5146f19f529d77ec83d3
arkeine <nils.ryter@he-arc.ch>
2015-11-19 18:34:26 +0100

    restore my binding

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -144,8 +144,10 @@ return [
         Dixit\Providers\AuthServiceProvider::class,
         Dixit\Providers\EventServiceProvider::class,
         Dixit\Providers\RouteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Dixit\Providers\BindingServiceProvider::class,
         
-       
+
     ],
 
     /*
@@ -194,10 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        
-        'Debugbar'  => Barryvdh\Debugbar\Facade::class,  
-        //'Form'  => Collective\Html\FormFacade::class,
-        
+        'DebugBar'  => Barryvdh\Debugbar\Facade::class,
+
     ],
 
 ];
