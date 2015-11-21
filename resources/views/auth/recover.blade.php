5d4530d63c8b350c364a371a8aaac05df57a940b
Romain Claret <contact@rocla.ch>
2015-11-21 21:36:41 +0100

    / Correction of the regex for the superficial validation

--- resources/views/auth/recover.blade.php
+++ resources/views/auth/recover.blade.php
@@ -105,7 +105,7 @@
 
     });
     function isEmail(email){
-  		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  		var regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.)+([a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
   		return regex.test(email);
 	}
 </script>
