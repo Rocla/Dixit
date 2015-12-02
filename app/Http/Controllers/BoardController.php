95375b1e0e708d498bb0e74291c1ecfafa8e1825
Romain Claret <contact@rocla.ch>
2015-12-02 19:34:14 +0100

    / Prototype Board, made design

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 use Dixit\Http\Requests;
 use Dixit\Http\Controllers\Controller;
 
+use Dixit\Card;
+
 class BoardController extends Controller
 {
+	protected $cards;
+
+	public function __construct(Card $_cards)
+	{
+		$this->cards=$_cards;
+		$this->middleware('auth');
+	}
+
     public function getIndex()
     {  
-        return view('board');  
+        return view('board')->with('cards', $this->cards->all());  
     }
 }
