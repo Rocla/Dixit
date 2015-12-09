6a1072819a49f1f2f73672d518fd06ca1fb43c87
Romain Claret <contact@rocla.ch>
2015-12-09 23:46:00 +0100

    + Added some missing .gitignore / Readme for the incoming game server / Drag&Drop on the board upgraded, you can now exchange cards before validate the game / Board layout is changing depending on where the player is sitting

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -31,73 +31,77 @@
   
                         <p id="game_status"></p>
                     </div>
-                    
+                    <div id="top_game_actions"></div>
+                    <div id="top_game_hand"></div>
                     <div>
                         <div>
-                            <div class="card_on_board" id="spot_card_12">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_12">
                             </div>
-                            <div class="card_on_board" id="spot_card_11">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_11">
                             </div>
-                            <div class="card_on_board" id="spot_card_10">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_10">
                             </div>
-                            <div class="card_on_board" id="spot_card_9">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_9">
                             </div>
-                            <div class="card_on_board" id="spot_card_8">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_8">
                             </div>
-                            <div class="card_on_board" id="spot_card_7">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_7">
                             </div>
                         </div>
 
                         <img src="{{asset('/images/boards/theme_odyssey.jpg')}}" width="100%">
 
                         <div>
-                            <div class="card_on_board" id="spot_card_1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-                            <div class="card_on_board" id="spot_card_2">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_1">
                             </div>
-                            <div class="card_on_board" id="spot_card_3">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%"  id="spot_card_2">
                             </div>
-                            <div class="card_on_board" id="spot_card_4">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_3">
                             </div>
-                            <div class="card_on_board" id="spot_card_5">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_4">
                             </div>
-                            <div class="card_on_board" id="spot_card_6">
-                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%">
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_5">
+                            </div>
+                            <div class="card_on_board">
+                                <img src="{{asset('/images/default_avatar/waiting_light_blue.png')}}" width="100%" id="spot_card_6">
                             </div>
                         </div>
                     </div>
-                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                    
+                    <div id="bottom_game_hand">
                     Hand:
-                    <div class="well">
-                        <div class="hand"><center>
-                            <img id="drag_card_1" src="{{asset('/images/cards/official/')}}/carte_1.png" width="100%" draggable="true" ondragstart="drag(event)">
-                            <img id="drag_card_2" src="{{asset('/images/cards/official/')}}/carte_2.png" width="100%" draggable="true" ondragstart="drag(event)">
-                            <img id="drag_card_3" src="{{asset('/images/cards/official/')}}/carte_3.png" width="100%" draggable="true" ondragstart="drag(event)">
-                            <img id="drag_card_4" src="{{asset('/images/cards/official/')}}/carte_4.png" width="100%" draggable="true" ondragstart="drag(event)">
-                            <img id="drag_card_5" src="{{asset('/images/cards/official/')}}/carte_5.png" width="100%" draggable="true" ondragstart="drag(event)">
-                            <img id="drag_card_6" src="{{asset('/images/cards/official/')}}/carte_6.png" width="100%" draggable="true" ondragstart="drag(event)">
-                        </center></div>
+                        <div class="well">
+                            <div class="hand" id="hand_set">
+                                <img id="drag_card_1" src="{{asset('/images/cards/official/')}}/carte_1.png" width="100%" draggable="true" ondragstart="drag(event)">
+                                <img id="drag_card_2" src="{{asset('/images/cards/official/')}}/carte_2.png" width="100%" draggable="true" ondragstart="drag(event)">
+                                <img id="drag_card_3" src="{{asset('/images/cards/official/')}}/carte_3.png" width="100%" draggable="true" ondragstart="drag(event)">
+                                <img id="drag_card_4" src="{{asset('/images/cards/official/')}}/carte_4.png" width="100%" draggable="true" ondragstart="drag(event)">
+                                <img id="drag_card_5" src="{{asset('/images/cards/official/')}}/carte_5.png" width="100%" draggable="true" ondragstart="drag(event)">
+                                <img id="drag_card_6" src="{{asset('/images/cards/official/')}}/carte_6.png" width="100%" draggable="true" ondragstart="drag(event)">
+                            </div>
+                        </div>
                     </div>
-                    Actions:
-                    <div class="well" float="left">
-                        Timer: <p id="timer_turn"></p>
-                        You are the Storyteller:
-                        <center>
-                            <textarea id="story"></textarea><br/>
-                            <button type='button' id="create_new_turn" class="btn btn-primary" disabled>Validate new turn</button>
-                            <button type='button' id="validate_card" class="btn btn-primary" disabled>Validate card</button>
-                            <button type='button' id="reset_hand" class="btn btn-primary">Reset hand</button>
-                        </center>
-
+                    <div id="bottom_game_actions">
+                        Actions:
+                        <div class="well" float="left">
+                            Timer: <p id="timer_turn"></p>
+                            You are the Storyteller:
+                                <textarea id="story"></textarea><br/>
+                                <button type='button' id="create_new_turn" class="btn btn-primary" disabled>Validate new turn</button>
+                                <button type='button' id="validate_card" class="btn btn-primary" disabled>Validate card</button>
+                                <button type='button' id="reset_hand" class="btn btn-primary">Reset hand</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -105,42 +109,76 @@
     </div>
 </div>
 
-<style>
-
-#spot_card_1 {
-    background-image: url("{{asset('/images/cards/drag_card_here.png')}}");
-    background-size: 100% 100%;
-}
-
-</style>
 
 <script type="text/javascript"> 
 
+// Retrived data from controller at loading
+var player_spot_number = 5;
+var player_cards_number = 6;
+var validated_players = [];
+var players_playing = [];
+var player_owner = 1;
+var player_storyteller = 1;
+
+
+// Retrived data from live server
+var game_status = "";
+var game_started = false;
+var player_played = [];
+
+// Computated data
+var play_spot = "spot_card_" + player_spot_number;
+
+
+// On page load
 $(document).ready(function(){
     $imageRatio = 500/330;
     resize_height_cards();
     $( window ).resize(function() {
         resize_height_cards();
     });
+
+    $("#"+play_spot).attr('src', "{{asset('/images/cards/drag_card_here.png')}}");
+    $("#"+play_spot).attr('ondrop', 'drop(event)');
+    $("#"+play_spot).attr('ondragover', 'allowDrop(event)');
+
+    if(player_spot_number > 6)
+    {
+        document.getElementById('top_game_hand').appendChild(document.getElementById('bottom_game_hand'));
+        document.getElementById('top_game_actions').appendChild(document.getElementById('bottom_game_actions'));
+    }
 });
 
+// Drag and drop for player
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("id", ev.target.getAttribute('id'));
+    ev.dataTransfer.setData("src", ev.target.getAttribute('src'));
 }
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    // ev.target.appendChild(document.getElementById(data));
-    // ev.target.removeChild(ev.target.childNodes[0]);
-    ev.target.appendChild(document.getElementById(data));
+
+    var data = ev.dataTransfer.getData("id");
+    var data_url = ev.dataTransfer.getData("src");
+
+    if ($('#hand_set').children().length < player_cards_number)
+    {
+        document.getElementById(data).src = ev.target.getAttribute('src');
+    }
+    else
+    {
+        document.getElementById(data).remove();
+    }
+
+    document.getElementById(play_spot).src = data_url;
+
 }
   
-
+// Design of the cards on board
 function resize_height_cards(){
     $imageHeight = $(".card_on_board").width()*$imageRatio;
     $(".card_on_board").css('height', $imageHeight + 'px');
